a4c84a82861110ba29043777774a51dfe7faa30a
Minji Kim <minji_0801@naver.com>
2021-10-18 21:32:44 +0900

    브루어리 소개 앱 구현
    * URLSession을 이용한 Network 구현
    * PunkAPI
    * SnapKit, Kingfisher

---
+++ Brewery/Beer.swift
@@ -0,0 +1,32 @@
+//
+//  Beer.swift
+//  Brewery
+//
+//  Created by 김민지 on 2021/10/18.
+//
+
+import Foundation
+
+struct Beer: Decodable {
+    let id: Int?
+    let name, taglineString, description, brewersTips, imageURL: String?
+    let foodPairing: [String]?
+    
+    var tagLine: String {
+        let tags = taglineString?.components(separatedBy: ". ")
+        let hashtags = tags?.map {
+            "#" + $0.replacingOccurrences(of: " ", with: "")
+                .replacingOccurrences(of: ".", with: "")
+                .replacingOccurrences(of: ",", with: " #")
+        }
+        return hashtags?.joined(separator: " ") ?? ""
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description
+        case taglineString = "tagline"
+        case imageURL = "image_url"
+        case brewersTips = "brewers_tips"
+        case foodPairing = "food_pairing"
+    }
+}
